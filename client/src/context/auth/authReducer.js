d5ea304e0fb06a5fea73fd3190f5db412d85f4ef
Oliver <molivera@8amarketing.com>
2021-03-25 19:55:20 -0300

    Load User and Set Token

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -11,7 +11,15 @@ import {
 
 export default (state, action) => {
     switch(action.type) {
+        case USER_LOADED:
+            return{
+                ...state,
+                isAuthenticated: true,
+                loading: false,
+                user: action.payload
+            }
         case REGISTER_SUCCESS:
+            localStorage.setItem('token', action.payload.token);
             return {
                 ...state,
                 ...action.payload,
@@ -19,6 +27,8 @@ export default (state, action) => {
                 loading: false
             };
         case REGISTER_FAIL:
+        case AUTH_ERROR:
+            localStorage.removeItem('token');
             return {
                 ...state,
                 token: null,
